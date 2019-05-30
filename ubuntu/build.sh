0bf4683db8e476c525edc68835139488a98c0a43
Adam Suwała <adam.suwala@ewolwe.pl>
2019-05-30 21:16:41 +0200

    based on latest openjdk10

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-SOURCE_VERSION=x86_64-ubuntu-jdk-10.0.2.13
-BUILD_VERSION=1.2
+VERSION=1.3
 
-docker build . -t teamcity-minimal-agent-jdk10:$SOURCE_VERSION-$BUILD_VERSION
-docker tag teamcity-minimal-agent-jdk10:$SOURCE_VERSION-$BUILD_VERSION teamcity-minimal-agent-jdk10:latest
+docker build . -t teamcity-minimal-agent-jdk10:$VERSION
+docker tag teamcity-minimal-agent-jdk10:$VERSION teamcity-minimal-agent-jdk10:latest
