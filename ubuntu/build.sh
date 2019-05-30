59a215ab00a88f7d7d3a1f94febec6c46e3ebfaa
adam00s <adam000@autograf.pl>
2019-05-30 19:56:53 +0200

    jdk10

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SOURCE_VERSION=x86_64-ubuntu-jdk-11.0.3_7
-BUILD_VERSION=1.0
+SOURCE_VERSION=x86_64-ubuntu-jdk-10.0.2.13
+BUILD_VERSION=1.1
 
 docker build . -t teamcity-minimal-agent-jdk11:$SOURCE_VERSION-$BUILD_VERSION
 docker tag teamcity-minimal-agent-jdk11:$SOURCE_VERSION-$BUILD_VERSION teamcity-minimal-agent-jdk11:latest
