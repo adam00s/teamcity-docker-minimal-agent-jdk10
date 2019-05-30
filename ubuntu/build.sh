d3069fc15468885fd1a6759e73e1ec72062a6fd0
adam00s <adam000@autograf.pl>
2019-05-30 20:48:22 +0200

    jdk10

--- ubuntu/build.sh
+++ ubuntu/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SOURCE_VERSION=x86_64-ubuntu-jdk-10.0.2.13
-BUILD_VERSION=1.1
+BUILD_VERSION=1.2
 
-docker build . -t teamcity-minimal-agent-jdk11:$SOURCE_VERSION-$BUILD_VERSION
-docker tag teamcity-minimal-agent-jdk11:$SOURCE_VERSION-$BUILD_VERSION teamcity-minimal-agent-jdk11:latest
+docker build . -t teamcity-minimal-agent-jdk10:$SOURCE_VERSION-$BUILD_VERSION
+docker tag teamcity-minimal-agent-jdk10:$SOURCE_VERSION-$BUILD_VERSION teamcity-minimal-agent-jdk10:latest
